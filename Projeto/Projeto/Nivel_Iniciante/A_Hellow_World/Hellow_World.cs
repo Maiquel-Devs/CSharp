ed8c1fe472132c927a7fccb491693586386858aa
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-10-18 16:32:08 -0300

    Hellow Word em C#
    Usando o Visual Studio.

---
+++ Projeto/Projeto/Nivel_Iniciante/A_Hellow_World/Hellow_World.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Nivel_Iniciante.A_Hellow_World
+{
+    internal class Hellow_World
+    {
+        static void Main(string[] args)  
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
